a1bdb544f18bcd7eba746f09f7ce45bd6688db4f
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-12 20:36:28 -0300

    primeiro comit

---
+++ ex_01.py
@@ -0,0 +1,6 @@
+# 1) Desenvolva um programa que receba do usuário um valor numérico inteiro x e retorne como
+#   saída o resultado da seguinte fórmula: 𝑥² + 10𝑥 − 5
+
+numeroInteiro = int(input("Insira o número que deseja para entrar como a variável x na fórmula '𝑥² + 10𝑥 − 5':"))
+
+resultFormula = ((numeroInteiro ** numeroInteiro) ++ ((10 ** numeroInteiro) −-5

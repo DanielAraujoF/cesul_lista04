4be6574f96fe4face78ac66dc3dbd7dece9d1f25
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-12 22:03:02 -0300

    segundo commit

--- ex_02.py
+++ ex_02.py
@@ -2,4 +2,13 @@
 #   prestações pagas e o valor da prestação. Escreva um programa que determine o valor total pago
 #   pelo consorciado e o saldo devedor.
 
-totalParcelas = int("Olá ")
+totalParcelas = int(input("Quantas parcelas foi feito? "))
+valorParcela = int(input("Qual valor de cada parcela? R$"))
+parcelasPagas = int(input("Quantas parcelas já foram pagas? "))
+
+valorTotal = totalParcelas * valorParcela
+valorPago = parcelasPagas * valorParcela
+
+valorAPagar = valorTotal - valorPago
+
+print(f"Você já pagou R${valorPago:.0f}, ainda falta R${valorAPagar:.0f}.")

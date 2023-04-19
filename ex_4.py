ef3a7eca46d75fcb872b932bc7a073fb5470aafd
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-19 19:13:56 -0300

    segundo commit

---
+++ ex_4.py
@@ -0,0 +1,4 @@
+# 4) Faça um programa para calcular a quantidade de barbante necessária, em centímetros, para amarrar um pacote com
+# duas voltas de barbante sendo uma pela largura e outra pelo comprimento. Serão fornecidas a largura, a altura e o
+# comprimento do pacote. Para amarrar as pontas do barbante são necessários 10 cm de barbante em cada ponta.
+

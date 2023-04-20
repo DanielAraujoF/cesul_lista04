6b63e58c7d6611b0e2a44cf1c3cb021b8f98bc20
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-19 22:39:12 -0300

    commit message

--- ex_04.py
+++ ex_04.py
@@ -2,8 +2,16 @@
 # duas voltas de barbante sendo uma pela largura e outra pelo comprimento. Serão fornecidas a largura, a altura e o
 # comprimento do pacote. Para amarrar as pontas do barbante são necessários 10 cm de barbante em cada ponta.
 
-altura = float(input("Informe a altura da caixa: "))
-largura = float(input("Informe a largura da caixa: "))
-comprimento = float(input("Informe o comprimento da caixa: "))
+alt = float(input("Informe a altura da caixa: "))
+larg = float(input("Informe a largura da caixa: "))
+comp = float(input("Informe o comprimento da caixa: "))
 
+barbLarg = (alt * 2) + (larg * 2) + 10
+barbComp = (alt * 2) + (larg * 2) + 10
 
+totalBarb = barbComp + barbLarg
+
+print(f"O comprimento de barbante a ser usado é {totalBarb:.2f}")
+
+# no lugar das linhas 9-12, poderia ter sido feito assim:
+# totalBarbanteUsado = (((altura * 2) + (largura * 2)) + ((altura * 2) + (largura * 2))) + 20

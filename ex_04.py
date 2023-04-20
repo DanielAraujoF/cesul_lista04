aa6fca5f2c11df5e79d36ebd3c08422489f7cf4e
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-19 22:30:50 -0300

    commit message

--- ex_04.py
+++ ex_04.py
@@ -2,3 +2,8 @@
 # duas voltas de barbante sendo uma pela largura e outra pelo comprimento. Serão fornecidas a largura, a altura e o
 # comprimento do pacote. Para amarrar as pontas do barbante são necessários 10 cm de barbante em cada ponta.
 
+altura = float(input("Informe a altura da caixa: "))
+largura = float(input("Informe a largura da caixa: "))
+comprimento = float(input("Informe o comprimento da caixa: "))
+
+
